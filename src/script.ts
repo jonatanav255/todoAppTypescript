e71e1ffc67cce254af27d127ce1f88fff23af3c4
Jonatandatafiniti <jonatanav255@hotmail.com>
2023-08-15 20:34:44 -0600

    logic and some html changes
    also css

--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,46 @@
-console.log("hola");
-console.log("hola");
+const inputBox = document.getElementById("input-box") as HTMLInputElement;
+
+const listContainer = document.getElementById(
+  "list-container"
+) as HTMLUListElement;
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("you must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    const clickedElement = e.target as HTMLElement;
+    if (clickedElement.tagName === "LI") {
+      clickedElement.classList.toggle("checked");
+      saveData();
+    } else if (clickedElement.tagName === "SPAN") {
+      clickedElement.parentElement?.remove();
+      saveData();
+    }
+  },
+  false
+);
+function saveData() {
+  localStorage.set("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  const data = localStorage.getItem("data");
+  if (data !== null) {
+    listContainer.innerHTML = data;
+  }
+}
+showTask();
